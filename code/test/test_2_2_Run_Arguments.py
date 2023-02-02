64651a570020f996155336ae26bb963249f94719
MostHappyCougar <mosthappycougar@gmail.com>
2023-02-02 14:02:36 +0400

    Update test_2_2_Run_Arguments.py

--- code/test/test_2_2_Run_Arguments.py
+++ code/test/test_2_2_Run_Arguments.py
@@ -7,7 +7,7 @@ import pytest
 @allure.suite("2_2_Run_Arguments")
 @allure.sub_suite("2_2_2_Positive")
 @allure.tag("positive", "configurations", "run_arguments")
-@allure.feature("State-Transitions Diagram")
+@allure.feature("Configuration")
 @allure.severity(allure.severity_level.TRIVIAL)
 class TestPositive:
     
@@ -23,34 +23,32 @@ class TestPositive:
         #Path to output directory
         _path_to_out_f = os.path.join(_global_parameters.get_params()["relative_path"]["from_test_to_utility"], "output", "2_2_2_Positive", "case_1")
         _path_to_out_s = os.path.join(_global_parameters.get_params()["relative_path"]["from_test_to_utility"], "output", "2_2_2_Positive", "case_2")      
-        
-        if case_id:
-            _out_files_empty = os.path.join(_global_parameters.get_params()["relative_path"]["from_test_to_utility"], "output", case_id)
-            _output_files_e = output_manager.FilesManagement(_out_files_empty)
-            with allure.step("Preconditions"):
-                with allure.step("Flush output"):
-                    _output_files_e.remove_files(files=_output_files_list) 
-                    
+               
         with allure.step("Preconditions"):
             with allure.step("Flush output"):
-                _output_files_f = output_manager.FilesManagement(_path_to_out_f)
-                _output_files_s = output_manager.FilesManagement(_path_to_out_s) 
-                _output_files_f.remove_files(files=_output_files_list)
-                _output_files_s.remove_files(files=_output_files_list)
+                if case_id:
+                    _out_files_empty = os.path.join(_global_parameters.get_params()["relative_path"]["from_test_to_utility"], "output", case_id)
+                    _output_files_e = output_manager.FilesManagement(_out_files_empty)
+                    _output_files_e.remove_files(files=_output_files_list)
+                else:
+                    _output_files_f = output_manager.FilesManagement(_path_to_out_f)
+                    _output_files_s = output_manager.FilesManagement(_path_to_out_s) 
+                    _output_files_f.remove_files(files=_output_files_list)
+                    _output_files_s.remove_files(files=_output_files_list)
                 
         with allure.step("Run utility"):
             user_actions.User().try_to_run(run_arguments)
             
-            with allure.step("Postconditions"):
-                    with allure.step("Flush output files and validate if there was generated"):
-                        if run_arguments:
-                            with allure.step("First config"):
-                                _output_files_f.remove_files(files=_output_files_list, validate=True)
-                            with allure.step("First config"):
-                                _output_files_s.remove_files(files=_output_files_list, validate=True)
-                        else:
-                            with allure.step("Empty Config"):
-                                _output_files_e.remove_files(files=_output_files_list_empty_config, validate=True)
+        with allure.step("Postconditions"):
+                with allure.step("Flush output files and validate if there was generated"):
+                    if run_arguments:
+                        with allure.step("First config"):
+                            _output_files_f.remove_files(files=_output_files_list, validate=True)
+                        with allure.step("First config"):
+                            _output_files_s.remove_files(files=_output_files_list, validate=True)
+                    else:
+                        with allure.step("Empty Config"):
+                            _output_files_e.remove_files(files=_output_files_list_empty_config, validate=True)
                             
                         
 
