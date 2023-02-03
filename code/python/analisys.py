020e183cf6608759d0eeef12a73cd65de3c76373
MostHappyCougar <mosthappycougar@gmail.com>
2023-02-03 15:19:22 +0400

    Add sorting by sequencer

--- code/python/analisys.py
+++ code/python/analisys.py
@@ -10,6 +10,10 @@ class StateTransitionDiagram:
     '''
     
     def __init__(self, table_to_analizys:pd.DataFrame, sequencer:str, group_by:str, transitions:str, states:str, output_files:str, output_filenames:str):
+        
+        #Sort states and transitions by sequencer
+        table_to_analizys.sort_values(sequencer)
+        
         #Output files names
         self.__files = output_files
         self.__filenames = output_filenames
