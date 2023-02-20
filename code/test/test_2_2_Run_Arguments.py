6eb4af0d801c7651faaa2259d5c6210bee61b5eb
MostHappyCougar <mosthappycougar@gmail.com>
2023-02-20 13:33:34 +0400

    Add additional validation for tests
    Validation for both configs with SEVERAL analysis mods and SINGLE mod thats listed in SAME config list as a run arguments is added

--- code/test/test_2_2_Run_Arguments.py
+++ code/test/test_2_2_Run_Arguments.py
@@ -13,6 +13,10 @@ from common_methods import GLOBAL
 @allure.feature("Configuration")
 @allure.severity(allure.severity_level.TRIVIAL)
 class TestPositive:
+    '''
+    There is exist an additional validation for list of configs with SEVERAL analysis mods and SINGLE analysis mod per list
+    In onle config list exists two configs. One config contains a SINGLE analysis mod, another config contains SEVERAL analysis mods
+    '''
     
     os.path.join("TEST", "conf_2_2_2_1")
     
@@ -21,7 +25,8 @@ class TestPositive:
     def test_RunAgruments(self, case_id, run_arguments):
         
         #Output files list
-        _output_files_list = ["2_2_2_Positive_path_stats_vis.pdf", "2_2_2_Positive_path_stats.xlsx", "2_2_2_Positive.gv", "2_2_2_Positive.gv.pdf"]
+        _output_files_list_first = ["2_2_2_Positive_path_stats_vis.pdf", "2_2_2_Positive_path_stats.xlsx", "2_2_2_Positive.gv", "2_2_2_Positive.gv.pdf"]
+        _output_files_list_second = ["2_2_2_Positive_path_stats_vis.pdf", "2_2_2_Positive_path_stats.xlsx", "2_2_2_Positive.gv", "2_2_2_Positive.gv.pdf", "2_2_2_Positive_param_trace.xlsx"]
         _output_files_list_empty_config = ["EX_TEST_path_stats_vis.pdf", "EX_TEST_path_stats.xlsx", "EX_TEST.gv", "EX_TEST.gv.pdf", "EX_TEST_param_trace.xlsx"]
         
         #Path to output directory
@@ -37,8 +42,8 @@ class TestPositive:
                 else:
                     _output_files_f = files_processor.OutputManager(_path_to_out_f)
                     _output_files_s = files_processor.OutputManager(_path_to_out_s) 
-                    _output_files_f.delete_files(files=_output_files_list)
-                    _output_files_s.delete_files(files=_output_files_list)
+                    _output_files_f.delete_files(files=_output_files_list_first)
+                    _output_files_s.delete_files(files=_output_files_list_second)
                 
         with allure.step("Run utility"):
             _actual_artifacts = user.User().try_to_get_exit_artifacts(run_arguments)
@@ -49,9 +54,9 @@ class TestPositive:
                 with allure.step("Flush output files and validate if there was generated"):
                     if run_arguments:
                         with allure.step("Output of FIRST config"):
-                            _output_files_f.delete_files(files=_output_files_list, fail_if_absence=True)
+                            _output_files_f.delete_files(files=_output_files_list_first, fail_if_absence=True)
                         with allure.step("Output of SECOND config"):
-                            _output_files_s.delete_files(files=_output_files_list, fail_if_absence=True)
+                            _output_files_s.delete_files(files=_output_files_list_second, fail_if_absence=True)
                     else:
                         with allure.step("Empty Config"):
                             _output_files_e.delete_files(files=_output_files_list_empty_config, fail_if_absence=True)
